2cc580f3726cba106f46e04c980073042eaf1811
Divakar Verma <137818590+divakar-amd@users.noreply.github.com>
2025-01-07 10:51:53 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,3 +8,8 @@ cd  ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+
+cp .tmux.conf ~/
+tmux source-file ~/.tmux.conf
+
+cp .vimrc ~/
