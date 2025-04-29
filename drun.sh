8655f3754d78a2a21993f61d1702712ca16798aa
Divakar Verma <divakar.verma@amd.com>
2025-04-29 15:56:44 -0500

    add docker run script

---
+++ drun.sh
@@ -0,0 +1,10 @@
+
+gpu_args="--device /dev/kfd --device /dev/dri --group-add video" 
+mount_dirs="-v /data/:/data -v ${HOME}/Projects/:/Projects -v /mnt/:/mnt/"
+
+image_name=$1
+container_name=$2
+
+cmd="docker run --name ${container_name} -i -d  --network=host ${gpu_args} --cap-add=SYS_PTRACE --security-opt seccomp=unconfined ${mount_dirs} --shm-size=16G --ulimit memlock=-1 --ulimit stack=67108864 ${image_name}"
+echo ${cmd}
+${cmd}
