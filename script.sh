e9076a59d46c88f51426529ab425a4ec2e937585
Divakar Verma <divakar.verma@amd.com>
2025-04-29 15:09:39 -0500

    add tmux_rocm_smi

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+cp ./tmux_rocm_smi.sh ~/
+cp .tmux.conf ~/
+tmux source-file ~/.tmux.conf
+
+cp .vimrc ~/
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -9,7 +14,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 
-cp .tmux.conf ~/
-tmux source-file ~/.tmux.conf
-
-cp .vimrc ~/
