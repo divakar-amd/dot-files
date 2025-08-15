46e19d726404f3616a14c158fbf8e7b4c3f17b3b
Divakar Verma <divakar.verma@amd.com>
2025-08-15 18:59:51 -0500

    disable ohmyssh in the script

--- script.sh
+++ script.sh
@@ -10,13 +10,10 @@ cp .vimrc ~/
 cp drun.sh ~/.local/bin/drun
 chmod +x ~/.local/bin/drun
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-cd  ~/.oh-my-zsh/custom/plugins
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#cd  ~/.oh-my-zsh/custom/plugins
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 
 cat << 'EOF' >> ~/.bashrc
 alias dps='docker ps --format="table {{.ID}}\t{{printf \"%.50s\" .Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Names}}"'
